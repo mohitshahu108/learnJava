18de473a9afb2e56d5bfeab225c8c5ee74f7a73a
MohitShahu12 <Mohit@accrualify.com>
2023-07-16 06:32:48 +0530

    Add hello world program

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// jvm converts the compiled byte code into an executable file
+// jre is software package that holds component required for successful execution of codes
+// jdk comprises of jre and development toolkits
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
